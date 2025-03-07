29b66059fa94a7b32322ace046ab245ef7caf080
Andrii <148819392+andriikamanin@users.noreply.github.com>
2025-03-07 18:43:49 +0100

    загрузка сгенерированых spring проектов

---
+++ api-service/src/test/java/it/volta/ts/kamaninandrii/api_service/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.volta.ts.kamaninandrii.api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ worker-service/src/test/java/it/volta/ts/kamaninandrii/worker_service/WorkerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.volta.ts.kamaninandrii.worker_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
